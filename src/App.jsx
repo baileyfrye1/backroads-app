9e84ed234b0a46cdd0b6d7934953e056ce0c26ff
Bailey Frye <bailey@Baileys-MacBook-Pro.local>
2024-03-27 10:51:35 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import About from './components/About.jsx';
+import Footer from './components/Footer.jsx';
+import Hero from './components/Hero.jsx';
+import Navbar from './components/Navbar.jsx';
+import Services from './components/Services.jsx';
+import Tours from './components/Tours.jsx';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SocialLink.jsx
@@ -0,0 +1,23 @@
+import { socialLinks } from '../data';
+
+const SocialLink = ({ parentClass, itemClass }) => {
+  return (
+    <ul className={parentClass}>
+      {socialLinks.map(({ id, href, icon }) => {
+        return (
+          <li key={id}>
+            <a
+              href={href}
+              target='_blank'
+              rel='noreferrer'
+              className={itemClass}
+            >
+              <i className={icon}></i>
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+export default SocialLink;
